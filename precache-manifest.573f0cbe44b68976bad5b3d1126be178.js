a94cc7ea08ed7a837d99456a73708b970414279c
julianismaelberger <kontakt@julianberger.de>
2020-05-06 02:56:52 +0200

    Updates

---
+++ precache-manifest.573f0cbe44b68976bad5b3d1126be178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910aed296c955f2d282838fea023123e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "731c2f18594bd5919b45",
+    "url": "/todo/static/js/2.c240d6c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.c240d6c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d1c14a2dd25dbf2fb7",
+    "url": "/todo/static/js/main.db63128b.chunk.js"
+  },
+  {
+    "revision": "30e14d4da478f9c754f0",
+    "url": "/todo/static/js/runtime-main.806db1ba.js"
+  },
+  {
+    "revision": "5dfca99b8c80204e617bb0c5e3aa16f4",
+    "url": "/todo/static/media/todo_logo.5dfca99b.svg"
+  }
+]);
